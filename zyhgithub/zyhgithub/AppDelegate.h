4c962025fcfd70883d4db07c262df3e97ce02bef
[zhangyihao] <[15003125615@163.com]>
2015-08-27 18:40:14 +0800

    createproject

---
+++ zyhgithub/zyhgithub/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  zyhgithub
+//
+//  Created by 张益豪 on 15/8/27.
+//  Copyright (c) 2015年 张益豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ zyhgithub/zyhgithub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  zyhgithub
+//
+//  Created by 张益豪 on 15/8/27.
+//  Copyright (c) 2015年 张益豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
